8c49542e28dbd88cbcaad2cb3dcf0b52e80c367c
AndrMoura <andre_ptos29@msn.com>
2023-10-19 16:32:22 +0100

    test

--- index.js
+++ index.js
@@ -7,7 +7,21 @@ async function setupPlugin({ config }) {
     console.log(config.dialog_size)
 }
 
-// Plugin method that processes event
+async function splitText(dialog_text) {
+    
+    const userPattern = /user:(.*?)(?=(agent:|$))/gs;
+    const agentPattern = /agent:(.*?)(?=(user:|$))/gs;
+    
+    const userMatches = dialog_text.matchAll(userPattern);
+    const agentMatches = dialog_text.matchAll(agentPattern);
+
+    const userUtterances = [...userMatches].map(match => match[1]);
+    const agentUtterances = [...agentMatches].map(match => match[1]);
+    
+    return { user: userUtterances, agent: agentUtterances };
+
+}
+
 async function processEvent(event, { config, cache }) {
     if (!event.properties) {
         event.properties = {};
@@ -18,12 +32,10 @@ async function processEvent(event, { config, cache }) {
     }
 
     dialog = event.properties['text']
+    const utterances = splitText(dialog);
+    total_size = utterances.user.length + utterances.agent.length;
 
-    const userUtterances = dialog.match(/user:.*?(?=(agent:|$))/g) || [];
-    const agentUtterances = dialog.match(/agent:.*?(?=(user:|$))/g) || [];
-    const merged = userUtterances.concat(agentUtterances);
-
-    event.properties['dialog_size'] = merged.length;
+    event.properties['dialog_size'] = total_size;
     return event;
 }
 
