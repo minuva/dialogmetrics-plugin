53b2b19bac20f93e8e2d8a8120a8b4ad99341849
AndrMoura <andre_ptos29@msn.com>
2023-10-19 16:17:53 +0100

    tests

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 /* global module */
 /* eslint no-undef: "error" */
 
-async function splitText(dialog_text) {
-
-    console.log("splitting text", dialog_text)
-    // split text by "user:" or "agent:"
-    pattern = /user:|agent:/g
-    return dialog_text.split(pattern)
-}
 
 // Plugin method that runs on plugin load
 async function setupPlugin({ config }) {
@@ -25,9 +18,12 @@ async function processEvent(event, { config, cache }) {
     }
 
     dialog = event.properties['text']
-    texts = splitText(dialog);
-    event.properties['dialog_size'] = texts.length;
 
+    const userUtterances = dialog.match(/user:.*?(?=(agent:|$))/g) || [];
+    const agentUtterances = dialog.match(/agent:.*?(?=(user:|$))/g) || [];
+    const merged = userUtterances.concat(agentUtterances);
+
+    event.properties['dialog_size'] = merged.length;
     return event;
 }
 
