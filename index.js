64fb7cf4adee0cf797bf2b451aeb9d5dec1b28d2
AndrMoura <andre_ptos29@msn.com>
2023-10-20 14:49:16 +0100

    add makePostRequest function to index.js

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 /* global module */
 /* eslint no-undef: "error" */
-import { makePostRequest } from './apiRequests';
-
 const ROLE_USER = 'user';
 const ROLE_AGENT = 'agent';
 
 // Plugin method that runs on plugin load
 async function setupPlugin({ config }) {
     console.log(config.dialog_size)
+}
+
+async function makePostRequest(url, data) {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(data)
+      });
+  
+      if (response.status === 200) {
+        const responseData = await response.json();
+        return responseData;
+      } else {
+        console.error("Request failed with status code: " + response.status);
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
 }
 
 async function splitDialogText(dialog_text) {
