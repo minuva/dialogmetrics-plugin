ec3bb05c6bfb0ed04698f18f0ab3940186eaa08e
AndrMoura <andre_ptos29@msn.com>
2023-11-02 09:18:11 +0000

    new dialog format

--- index.js
+++ index.js
@@ -6,38 +6,24 @@ async function setupPlugin({ config }) {
     console.log(config)
 }
 
-async function splitDialogText(dialog_text) {
-    
-    const userPattern = /user:(.*?)(?=(agent:|$))/gs;
-    const agentPattern = /agent:(.*?)(?=(user:|$))/gs;
-    
-    const userMatches = dialog_text.matchAll(userPattern);
-    const agentMatches = dialog_text.matchAll(agentPattern);
-
-    const userUtterances = [...userMatches].map(match => match[1]);
-    const agentUtterances = [...agentMatches].map(match => match[1]);
-    
-    return { user: userUtterances, agent: agentUtterances };
-
-}
-
 async function processEvent(event, { config, cache }) {
 
     if (!event.properties) {
         event.properties = {};
     }
 
-    if (!event.properties['text']) {
+    if (!event.properties['$dialog']) {
         return event
     }
 
-    dialog = event.properties['text']
-    const utterances = await splitDialogText(dialog);
-    total_size = utterances.user.length + utterances.agent.length;
-
+    dialog = event.properties['$dialog']
+    console.log("before parsing dialog", dialog)
+    var dialog = JSON.parse(dialog);
+    console.log("after parsing dialog", dialog)
+    var dialogSize = dialogItems.length;
     // Calculate dialog size
-    event.properties['dialog_size'] = total_size;
-    console.log("total_size", total_size)
+    event.properties['dialog_size'] = dialogSize;
+    console.log("total_size", dialogSize)
     return event;
 }
 
